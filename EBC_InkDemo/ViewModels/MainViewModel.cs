cf8ddfefa56e3e5be8aded91a8f681f5c1a74ce2
JeffBrand <jbrand@microsoft.com>
2018-10-04 10:44:15 -0500

    fetching files

--- EBC_InkDemo/ViewModels/MainViewModel.cs
+++ EBC_InkDemo/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,35 @@ namespace EBC_InkDemo.ViewModels
     public class MainViewModel : AsyncViewModel
     {
 
-        protected override Task InitializeAsync()
+        private ObservableCollection<string> _samples = new ObservableCollection<string>();
+        public ReadOnlyObservableCollection<string> Samples { get; private set; }
+
+        public MainViewModel()
         {
-            return base.InitializeAsync();
+            Samples = new ReadOnlyObservableCollection<string>(_samples);
+        }
+
+        protected override async Task InitializeAsync()
+        {
+            await LoadSampleGalleryAsync("AzureArchitect");
+        }
+
+        private async Task LoadSampleGalleryAsync(string galleryName)
+        {
+            _samples.Clear();
+            try
+            {
+                var assets = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets"); ;
+                var folder = await assets.GetFolderAsync(galleryName);
+                var files = await folder.GetFilesAsync();
+                foreach (var file in files)
+                    _samples.Add(file.Path);
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
         }
     }
 }
